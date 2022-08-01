14d1007eee2543a9b3e0276b248ba8509354f3aa
leo870823 <leo870823@ln01-twnia2.twcc.ai>
2022-08-01 15:23:52 +0800

    First

---
+++ README.md
@@ -0,0 +1,16 @@
+# High-Performance-Computing-Tutorial
+國家高速網路中心使用方式整理
+
+## Build Environments 
+
+    bash gen_env.sh
+
+## Run Toy Example 
+
+
+    python3 mnist.py
+
+## Submit script
+
+	
+    sbatch test.sh
---
+++ gen_env.sh
@@ -0,0 +1,6 @@
+module purge
+module load miniconda3  cuda/10.2 
+conda create --name test python=3.8
+conda install pytorch==1.10.0 torchvision==0.11.0 torchaudio==0.10.0 cudatoolkit=10.2 -c pytorch
+conda activate test
+
